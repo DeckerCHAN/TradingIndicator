ed8bee064b2f066724ab612aa98d4bcb0e5a8ce9
DeckerCHAN <DeckerCHAN@gmail.com>
2016-08-01 22:32:46 +1000

    Code framework.

---
+++ src/main/java/com/deckerchan/tradingIndicator/api/APIManager.java
@@ -0,0 +1,38 @@
+package com.deckerchan.tradingIndicator.api;
+
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+public class APIManager extends HttpServlet {
+
+
+    public APIManager() {
+        super();
+    }
+
+    @Override
+    public void init() throws ServletException {
+        super.init();
+    }
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        resp.setContentType("text/html");
+        PrintWriter out = resp.getWriter();
+
+        out.println("test message");
+        out.flush();
+        out.close();
+
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        super.doPost(req, resp);
+    }
+}
