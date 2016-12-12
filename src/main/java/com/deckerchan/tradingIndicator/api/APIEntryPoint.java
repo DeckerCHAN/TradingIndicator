cd6cca95bccd06c3a060bef82a3eb05af03e3ee7
DeckerCHAN <DeckerCHAN@gmail.com>
2016-12-12 21:35:29 +1100

    Fixed changed result model.

--- src/main/java/com/deckerchan/tradingIndicator/api/APIEntryPoint.java
+++ src/main/java/com/deckerchan/tradingIndicator/api/APIEntryPoint.java
@@ -2,6 +2,7 @@ package com.deckerchan.tradingIndicator.api;
 
 
 import com.deckerchan.tradingIndicator.PresetConfiguration;
+import com.deckerchan.tradingIndicator.protocol.ErrorResult;
 import com.deckerchan.tradingIndicator.protocol.Result;
 import com.google.gson.Gson;
 
@@ -39,13 +40,30 @@ public class APIEntryPoint extends HttpServlet {
     protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
         String requestURI = req.getRequestURI();
         Result result;
+
+        String apiPath;
+        APIBase apiBase;
+
         try {
-            String apiPath = requestURI.substring(requestURI.lastIndexOf(PresetConfiguration.API_URL_PREFIX));
-            APIBase apiBase = APIManager.getManager().getAPI(apiPath);
+            apiPath = requestURI.substring(requestURI.lastIndexOf(PresetConfiguration.API_URL_PREFIX));
+            apiBase = APIManager.getManager().getAPI(apiPath);
+        } catch (Exception ex) {
+
+            result = new ErrorResult("Invalid path! Api not found.", ex);
+            resp.getWriter().write(gson.toJson(result));
+            return;
+        }
+
+
+
+        try {
+            apiBase.execute(req.getParameterMap());
             result = apiBase.getResult();
         } catch (Exception ex) {
-            result = new Result();
-            result.setErrorMessage(ex.getMessage());
+
+            result = new ErrorResult("Error occurred during execution.", ex);
+            resp.getWriter().write(gson.toJson(result));
+            return;
         }
 
         resp.getWriter().write(gson.toJson(result));
